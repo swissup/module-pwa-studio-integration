810ebc5a76ec137d21c92e4e3e095981da458c6a
Alexander Kras'ko <0m3r.mail@gmail.com>
2021-09-29 11:12:06 +0300

    Add getThemeDataByStore graph Api

---
+++ Model/Resolver/ThemeDataResolver.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types=1);
+
+namespace Swissup\PwaStudioIntegration\Model\Resolver;
+
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+
+use \Magento\Framework\App\Config\ScopeConfigInterface;
+use \Magento\Store\Model\StoreManagerInterface;
+use \Magento\Framework\View\Design\Theme\ThemeProviderInterface;
+
+class ThemeDataResolver implements ResolverInterface
+{
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     * @param StoreManagerInterface $storeManager
+     * @param ThemeProviderInterface $themeProvider
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        StoreManagerInterface $storeManager,
+        ThemeProviderInterface $themeProvider
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->storeManager = $storeManager;
+        $this->themeProvider = $themeProvider;
+    }
+
+    /**
+     * @return \Magento\Framework\View\Design\ThemeInterface
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    private function getTheme()
+    {
+        $themeId = $this->scopeConfig->getValue(
+            \Magento\Framework\View\DesignInterface::XML_PATH_THEME_ID,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            $this->storeManager->getStore()->getId()
+        );
+
+        /** @var $theme \Magento\Framework\View\Design\ThemeInterface */
+        $theme = $this->themeProvider->getThemeById($themeId);
+
+        return $theme;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(
+        Field $field,
+              $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        $data = $this->getTheme()->getData();
+        $data['store_id'] = $this->storeManager->getStore()->getId();
+        return $data;
+    }
+}
